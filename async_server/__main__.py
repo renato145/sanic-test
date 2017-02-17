29a889fa6bec9a9d4b1132234e5c09c6698f7537
Renato <renato145@hotmail.com>
2017-02-17 08:57:02 -0500

    async test

--- async_server/__main__.py
+++ async_server/__main__.py
@@ -1,11 +1,21 @@
 from . import app_model
+from time import time
 from sanic import Sanic
 from sanic.response import text, json
 from sanic.exceptions import NotFound, ServerError
 
+import asyncio
+import uvloop
+# from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
+
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+loop = asyncio.get_event_loop()
+# pool = ProcessPoolExecutor()
+# pool = ThreadPoolExecutor()
+
 model = None
 app = Sanic(__name__)
-
+i = 0
 
 def load_model():
     global model
@@ -48,33 +58,59 @@ async def i_am_ready_to_die(request):
 
 @app.route('/get-label')
 async def get_labels_from_url(request):
+    global i
+    # global pool
+    global loop
     global model
+    
+    i += 1
+    idx = i
+    print('Launching task %d' % idx)
+    t0 = time()
     if 'url' in request.args.keys():
         src = request.args['url'][0]
-        out = app_model.get_pred(src, model, 'url')
+        opt = 'url'
+        
     elif 'path' in request.args.keys():
         src = request.args['path'][0]
-        out = app_model.get_pred(src, model, 'path')
+        opt = 'path'
     else:
          return text('No url or path parameter.')
 
-    return json(out)
+    # out = await loop.run_in_executor(None, app_model.get_pred, src, model, opt, idx)
+    out = loop.run_in_executor(None, app_model.get_pred, src, model, opt, idx)
+    
+    
+    print('Task %d took %.2fs' % (idx, (time() - t0)))
+
+    return text('asdasd')
+    # return json(out)
 
 @app.route('/get-label', methods=['POST'])
 async def get_labels_from_file(request):
     global model
+    global i
+    
+    i += 1
+    idx = i
+    print('Launching task %d' % idx)
+    t0 = time()
     img_file = request.files.get('image')
+
     if img_file is None:
         return text('No image file found.')
 
-    out = app_model.get_pred(img_file.body, model, 'file')
+    out = app_model.get_pred(img_file.body, model, 'file', idx)
+    # loop.run_in_executor(pool, some_method, method_args)
+    print('Task %d took %.2fs' % (idx, (time() - t0)))
+
     return json(out)
 
 @app.exception(NotFound)
 async def ignore_404s(request, exception):
     return text("Yep, I totally found the page: %s" % request.url)
 
-app.run(host='0.0.0.0', port=8000, before_start=load_model(), debug=True)
+app.run(host='0.0.0.0', port=8000, before_start=load_model(), loop=loop, debug=True)
 # app.run(host='0.0.0.0', port=8000, debug=True)
 
 
---
+++ async_server/app_model.py
@@ -0,0 +1,42 @@
+import cv2
+import imutils
+import numpy as np
+import matplotlib.pyplot as plt
+from time import time
+from PIL import Image as pil_image
+from keras.preprocessing import image
+from keras.applications.inception_v3 import InceptionV3, preprocess_input, decode_predictions
+
+IMG_SIZE = (299, 299)
+
+
+def load_model():
+	t0 = time()
+	model = InceptionV3()
+	print('Model loaded (%.2fs).' % (time() - t0))
+
+	return model
+
+
+def get_pred(src, model, file_type, idx, img_size=IMG_SIZE):
+    if file_type == 'file':
+    	x = np.asarray(bytearray(src), dtype="uint8")
+    	x = cv2.imdecode(x, cv2.IMREAD_COLOR)
+    	x = cv2.cvtColor(x, cv2.COLOR_BGR2RGB)
+    elif file_type == 'url':
+        x = imutils.url_to_image(src)
+        x = cv2.cvtColor(x, cv2.COLOR_BGR2RGB)
+    elif file_type == 'path':
+        x = plt.imread(src)
+    else:
+    	return 'Invalid file type.'
+                
+    x = cv2.resize(x, img_size)
+    x = image.img_to_array(x)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    y = model.predict(x)
+    preds = decode_predictions(y)
+    preds = {l:float(p) for _, l, p in preds[0]}
+    print('ended task #%d' % idx)
+    return preds
